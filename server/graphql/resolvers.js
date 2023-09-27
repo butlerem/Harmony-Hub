49daad15f964fce22a351d93251754b0cf9b07b5
VithusanU <130177580+VithusanU@users.noreply.github.com>
2023-09-27 18:41:37 -0400

    working on resolvers.js

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,5 +1,4 @@
-// import typeDefs and resolvers
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
-// export typeDefs and resolvers
-module.exports = { typeDefs, resolvers };
+const { AuthentificationError } = require("apollo-server-express");
+const { user,project } = require('../models');
+const { signToken } = require('../utils/auth');
+const stripe = require('stripe')(pk_test_51Nv4fhEbTxDIrmjgqjURiHGZrjHNQFBBw5TzwLwxslF7Ps9HB55bpiMsKNzi4geETp37elheS45LCq6iCV0hC8xL00wypUDiSM)
