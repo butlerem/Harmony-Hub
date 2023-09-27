2dd7a3689787417c5b23a9fa63765cdd986cc5d6
VithusanU <130177580+VithusanU@users.noreply.github.com>
2023-09-27 16:33:04 -0400

    Added auth.js file

---
+++ server/middleware/auth.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+const secretKey = 'harmonyhubrules'; // Replace with your secret key
+const expiration = '2hr';
+
+
+module.exports = {
+    authMiddleware: function ({ req }) {
+      // allows token to be sent via req.body, req.query, or headers
+      let token = req.body.token || req.query.token || req.headers.authorization;
+  
+      // ["Bearer", "<tokenvalue>"]
+      if (req.headers.authorization) {
+        token = token.split(' ').pop().trim();
+      }
+  
+      if (!token) {
+        return req;
+      }
+  
+      try {
+        const { data } = jwt.verify(token, secretKey, { maxAge: expiration });
+        req.user = data;
+      } catch {
+        console.log('Invalid token');
+      }
+  
+      return req;
+    },
+    signToken: function ({ firstName, email, _id }) {
+      const payload = { firstName, email, _id };
+  
+      return jwt.sign({ data: payload }, secretKey, { expiresIn: expiration });
+    },
+  };
+  
