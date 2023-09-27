201aaee29d34396f0589a818a9c4062ed4d0a25f
VithusanU <130177580+VithusanU@users.noreply.github.com>
2023-09-27 16:44:35 -0400

    correctly moved index info to newly created index.js

---
+++ server/graphql/index.js
@@ -0,0 +1,5 @@
+// import typeDefs and resolvers
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+// export typeDefs and resolvers
+module.exports = { typeDefs, resolvers };
