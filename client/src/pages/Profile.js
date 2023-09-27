a598087383a414751706436ec26eed02b8e86b61
KatoCambly <katcambly@icloud.com>
2023-09-27 19:32:48 -0400

    fixed a spelling error - I'll leave the rest of the signup stuff to the backend devs

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,3 +1,5 @@
+//requires user profile, account settings, project history, and logout
+
 import React from 'react';
 import {
   ChakraProvider,
@@ -16,34 +18,25 @@ import {
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import { Navbar } from './components/Header';
 
 function Profile() {
   return (
     <ChakraProvider theme={theme}>
+      <Navbar />
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
             <Text> Your Profile </Text>
-            <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+<SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
   <Card>
     <CardHeader>
       <Heading size='md'> Projects </Heading>
     </CardHeader>
     <CardBody>
       <Text>View all of your projects. </Text>
-    </CardBody>
-    <CardFooter>
-      <Button>View here</Button>
-    </CardFooter>
-  </Card>
-  <Card>
-    <CardHeader>
-      <Heading size='md'> Collaborations </Heading>
-    </CardHeader>
-    <CardBody>
-      <Text>View all of your collaborations.</Text>
     </CardBody>
     <CardFooter>
       <Button>View here</Button>
