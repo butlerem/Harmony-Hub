81543d31da63277d1a68f33849f25efc0ff70b66
mahmud khan deep <mahmudkhan_deep@yahoo.com>
2023-09-28 19:54:02 -0400

    fornt dev homepage

---
+++ client/src/Components/Banner.js
@@ -0,0 +1,52 @@
+import React from "react";
+import artist from "../img/drakes-gods-plan.jpg"; 
+import check from "../img/check.png";
+import { FaEllipsisH, FaHeadphones, FaCheck } from "react-icons/fa";
+function Banner() {
+  return (
+    <div className="Banner">
+      <img src={artist} alt="" className="bannerImg" />
+
+      <div className="content">
+        <div className="breadCrump">
+          <p>
+            Home <span>/Popular Artist</span>
+          </p>
+          <i>
+            <FaEllipsisH />
+          </i>
+        </div>
+
+        <div className="artist">
+          <div className="left">
+            <div className="name">
+              <h2>Drake</h2>
+              <img src={check} alt="" />
+            </div>
+
+            <p>
+              <i>
+                <FaHeadphones />
+              </i>
+              100,484,0654 <span>Monthly Listeners</span>
+            </p>
+          </div>
+
+          <div className="right">
+            <a href="#"> Play</a>
+            <a href="#">
+              <i>
+                <FaCheck />
+              </i>
+              Following
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="bottom"></div>
+    </div>
+  );
+}
+
+export { Banner };
