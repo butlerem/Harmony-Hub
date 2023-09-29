9181e9ad5ccc18c14484d4d209b3370406cb5ed8
butlerem <butlerem01@gmail.com>
2023-09-28 20:11:47 -0400

    Implement RESTful routes for projects

--- server/routes/project.js
+++ server/routes/project.js
@@ -12,4 +12,44 @@ router.post('/create', async (req, res) => {
   }
 });
 
+// List All Projects
+router.get('/list', async (req, res) => {
+  try {
+    const projects = await Project.find({});
+    res.json(projects);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get Single Project
+router.get('/:id', async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.id);
+    res.json(project);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update Project
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedProject = await Project.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedProject);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete Project
+router.delete('/:id', async (req, res) => {
+  try {
+    await Project.findByIdAndRemove(req.params.id);
+    res.json({ message: 'Project deleted' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
