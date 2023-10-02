9645228eaee325c4f4e93c48d583b2816f02958b
butlerem <butlerem01@gmail.com>
2023-10-02 15:15:41 -0400

    Testing

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const projectRoutes = require('./routes/project');
 
+app.use(express.json()); // For parsing JSON request body
 app.use('/api/project', projectRoutes);
 
-mongoose.connect('mongodb://localhost:27017/yourDatabaseName', {
+mongoose.connect('mongodb://localhost:27017/harmonyHubDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
