b454e3fd7fc388b5ace57d54b7a3b8f5c9ddc597
butlerem <butlerem01@gmail.com>
2023-10-02 17:50:42 -0400

    Debugging

--- server/routes/project.js
+++ server/routes/project.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Project = require('../models/Project');
-const User = require('../models/User');
+const Project = require('../models/project');
+const User = require('../models/user');
 
 // Create Project and Add to User
 router.post('/create', async (req, res) => {
